c4d8220ceffbd7bec01dda113b4e676aa2bfce49
HronovychOleh <hronovycholeh92@gmail.com>
2025-10-03 18:47:55 +0300

    chore: remove unnecesery uid generation

--- src/utils/generateUserId.ts
+++ src/utils/generateUserId.ts
@@ -2,23 +2,17 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const USER_ID_KEY = "@chat_user_id";
 
-const generateRandomUserId = (): string => {
-  return `User_${Math.random().toString(36).slice(2, 10)}`;
-};
-
 export const getUserId = async (): Promise<string> => {
   try {
     const existingUserId = await AsyncStorage.getItem(USER_ID_KEY);
 
     if (existingUserId) {
       return existingUserId;
+    } else {
+      throw new Error("User ID not found");
     }
-
-    const newUserId = generateRandomUserId();
-    await AsyncStorage.setItem(USER_ID_KEY, newUserId);
-    return newUserId;
   } catch (error) {
     console.error("Error managing user ID:", error);
-    return generateRandomUserId();
+    return "";
   }
 };
